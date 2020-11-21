cd0da4e48caea325efa3e042ffe5a09a92e74a5f
Hunter4 <stuhunter4@users.noreply.github.com>
2020-11-20 23:07:17 -0800

    finished part 1 of the homework, clean with comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,11 @@
 // from data.js, assign data to a descriptive variable
 var tableData = data;
 
-
 // select the button
 var button = d3.select("#filter-btn");
 
 // select the form
 var form  = d3.select("#form");
-
 
 // create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
@@ -24,39 +22,33 @@ function runEnter() {
 
     // get the value property of the input element
     var inputValue = inputElement.property("value");
-
     // test
     console.log(inputValue);
     console.log(tableData);
 
     // filter() uses input as its argument
     var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-
     // test
     console.log(filteredData);
 
-    // create arrays with the data key values
-    //var dates = filteredData.map(ufo => ufo.datetime);
-    //var cities = filteredData.map(ufo => ufo.city);
-    //var states = filteredData.map(ufo => ufo.state);
-    //var countries = filteredData.map(ufo => ufo.country);
-    //var shapes = filteredData.map(ufo => ufo.shape);
-    //var duration = filteredData.map(ufo => ufo.durationMinutes);
-    //var comment = filteredData.map(ufo => ufo.comments);
-
     // create a reference to the table body
     var tbody = d3.select("tbody");
-
     // remove any children from the table
     tbody.html("");
 
-    // use arrow functions to update each cell's text with filtered values
-    filteredData.forEach((ufo) => {
+    // loop through filtered data and console.log each report object
+    filteredData.forEach(function(ufoReport) {
+        console.log(ufoReport);
+        // append one table row 'tr' for each report object
         var row = tbody.append("tr");
-        Object.defineProperties(ufo).forEach(([key, value]) => {
+
+        // use 'Object.entries' to console.log each report value
+        Object.entries(ufoReport).forEach(function([key, value]) {
+            console.log(key, value);
+            // use d3 to append 1 cell for each value in the report object (column)
             var cell = row.append("td");
+            // use d3 to update each cell's text with report values
             cell.text(value);
         });
     });
-
 };
